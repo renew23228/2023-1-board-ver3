aa41a4e5eeb1e3624992131efe62e52013195c00
김재경 <worud4227@naver.com>
2023-05-30 10:49:31 +0900

    kjk controller Service entity

---
+++ src/main/java/com/green/boardver3/UserEntity.java
@@ -0,0 +1,16 @@
+package com.green.boardver3;
+
+import lombok.Data;
+
+@Data
+public class UserEntity {
+    private int iuser;
+    private String uid;
+    private String upw;
+    private String nm;
+    private char gender;
+    private String addr;
+    private String mainPic;
+    private String createdAt;
+    private String updateAt;
+}
---
+++ src/main/java/com/green/boardver3/UserService.java
@@ -0,0 +1,8 @@
+package com.green.boardver3;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+}
