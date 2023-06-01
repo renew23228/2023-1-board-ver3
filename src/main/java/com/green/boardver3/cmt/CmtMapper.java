a4c067e8516c3882d0773f9f5481a957e9e56456
배서윤 <qaz197@naver.com>
2023-06-01 12:14:02 +0900

    댓글페이지보기

--- src/main/java/com/green/boardver3/cmt/CmtMapper.java
+++ src/main/java/com/green/boardver3/cmt/CmtMapper.java
@@ -2,9 +2,14 @@ package com.green.boardver3.cmt;
 
 import com.green.boardver3.cmt.model.CmtEntity;
 import com.green.boardver3.cmt.model.CmtInsDto;
+import com.green.boardver3.cmt.model.CmtSelDto;
+import com.green.boardver3.cmt.model.CmtVo;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 @Mapper
 public interface CmtMapper {
     int insBoardCmt(CmtEntity entity);
+    List<CmtVo> selBoardCmt(CmtSelDto dto);
 }
--- src/main/java/com/green/boardver3/cmt/CmtService.java
+++ src/main/java/com/green/boardver3/cmt/CmtService.java
@@ -2,8 +2,12 @@ package com.green.boardver3.cmt;
 
 import com.green.boardver3.cmt.model.CmtEntity;
 import com.green.boardver3.cmt.model.CmtInsDto;
+import com.green.boardver3.cmt.model.CmtSelDto;
+import com.green.boardver3.cmt.model.CmtVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CmtService {
@@ -14,13 +18,7 @@ public class CmtService {
         this.MAPPER = MAPPER;
     }
 
-    int insBoardCmt(CmtInsDto dto) {
-        CmtEntity entity = new CmtEntity();
-        entity.setIboard(dto.getIboard());
-        entity.setIuser(dto.getIuser());
-        entity.setCtnt(dto.getCtnt());
-
-
+    int insBoardCmt(CmtEntity entity) {
         try {
             int result = MAPPER.insBoardCmt(entity);
 
@@ -35,4 +33,11 @@ public class CmtService {
     //w중간에 엔터티로 갈아탄이유? 방금입력된 레코드의 pk값을 얻기 위해 iboardCmt를 담을수있는 공간이 필요했고, 굳이 엔터티를
     //만든 이유는 나중에 jpa 에서 그렇게 하기때문이기도 하고 entity는 테이블과 무조건 동일하게 일대일 매핑되게 들어지는 개체이고
     //iboardCmt, 나중에 우리가 엔터티를 만들거다 왜? 이거보고 나중에 테이블을 만들거다
+
+
+    public List<CmtVo> selBoardCmt(CmtSelDto dto) {
+        int startIdx = (dto.getPage() -1) * dto.getRow();
+        dto.setStartIdx(startIdx);
+        return MAPPER.selBoardCmt(dto);
+    }
 }
